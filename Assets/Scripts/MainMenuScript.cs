25b87fa12b2a54f954ef9d807174a4e7e2cf732d
Amusselwhite <amusselwhite6@gatech.edu>
2016-03-31 21:57:37 -0400

    Start Screen
    Added a Start Screen with 3 buttons, start currently loads larrys Test
    Scene using the interact method from the elevator script but its on
    click, this should probably have a background?

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour {
+	public string sceneName;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	public void interact()
+	{
+		SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
